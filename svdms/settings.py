b11cb0d88632242e7e2da6b6af8529ce9b8cea12
Trung Pham <phamtientrung255@gmail.com>
2020-06-22 22:07:32 +0700

    add static path settings

--- svdms/settings.py
+++ svdms/settings.py
@@ -16,6 +16,9 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static root for static files like img, html, ..etc
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,7 +29,7 @@ SECRET_KEY = 'd@gpw1i_2gecs&iz*0+v3c4-gr_@5m$!zyr1#9jmr-&j1jg3e_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
