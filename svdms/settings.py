b0cf30073d6faa00eefe0dabb54fcbbd9e39644d
Trung Pham <phamtientrung255@gmail.com>
2020-06-22 22:56:31 +0700

    update gitignore

--- svdms/settings.py
+++ svdms/settings.py
@@ -12,9 +12,14 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv_file = os.path.join(BASE_DIR, ".env.dev")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Static root for static files like img, html, ..etc
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
