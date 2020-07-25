6dfb4955855ce021cbb70327df391872aedccc48
Trung Pham <phamtientrung255@gmail.com>
2020-07-25 07:13:30 +0700

    fix 500 status code when req GET /api/v1/login/

--- svdms/settings.py
+++ svdms/settings.py
@@ -94,7 +94,9 @@ ROOT_URLCONF = 'svdms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
